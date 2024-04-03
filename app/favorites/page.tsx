2e0281bd079ddc39ceca707dbfa053f1304e157a
Philip Bendiksen <philipbendiksen@gmail.com>
2024-04-03 15:46:22 +0200

    changes on titles

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -7,8 +7,8 @@ export default function Favorites() {
   const { movieData } = useMovieContext();
   return (
     <main className="lg:ml-28">
-      <h1>Favorites</h1>
-      <div className="ml-6 grid grid-cols-2 gap-4 md:grid-cols-4 lg:grid-cols-3">
+      <h1 className=" flex justify-center lg:text-4xl md:text-3xl text-2xl">Favorites</h1>
+      <div className=" m-6 grid grid-cols-2 gap-4 md:grid-cols-4 lg:grid-cols-3">
         {movieData.map(
           (movie: any) =>
             movie.bookmark && <MovieCard key={movie.title} {...movie} />
@@ -17,3 +17,4 @@ export default function Favorites() {
     </main>
   );
 }
+
--- components/Recommended.tsx
+++ components/Recommended.tsx
@@ -6,11 +6,11 @@ import MovieCard from "./MovieCard";
 export default function Recommended() {
   const { movieData } = useMovieContext();
   return (
-    <div className="flex flex-col">
-      <h1 className="ml-6 flex justify-center text-4xl">
+    <div className="lg:ml-28">
+      <h1 className=" m-12 flex justify-center lg:text-3xl md:text-2xl text-2xl">
         Recommended movies for you
       </h1>
-      <div className="ml-6 grid grid-cols-2 gap-4 md:grid-cols-4 lg:grid-cols-3">
+      <div className="ml-6 mb-6 grid grid-cols-2 gap-4 md:grid-cols-4 lg:grid-cols-3">
         {movieData
           .slice(movieData.length - 8, movieData.length)
           .map(
